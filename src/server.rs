6c48e7cd610e71ae82c7dda77a4ee3ee2b5cd96a
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-12-28 13:01:53 +0530

    Handle PAD response and writeback to client

--- src/server.rs
+++ src/server.rs
@@ -2,13 +2,32 @@ use tokio::net::{UnixStream, unix::SocketAddr};
 use serde::{Serialize, Deserialize};
 use std::io;
 use crate::config::{Config, Handler};
-use crate::pad;
-use tracing::{info, error, debug};
+use crate::pad::{PadRequest, PadResponse};
+use tracing::{info, error, debug, warn};
 
 #[derive(Serialize,Deserialize,Debug)]
 pub enum HardwareRequest {
     MotorWrite{motor: String, command: Vec<u8>},
     EncoderRead{encoder: String},
+}
+#[derive(Serialize,Deserialize,Debug)]
+pub enum HardwareResponse {
+    EncoderValue(i32),
+    Ok,
+}
+impl HardwareResponse {
+    pub fn from_pad_response(pr: PadResponse) -> Self {
+        match pr {
+            PadResponse::EncoderValue(v) => Self::EncoderValue(v),
+            PadResponse::Ok => Self::Ok,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ServerChannels {
+    send_to_pad: tokio::sync::mpsc::Sender<PadRequest>,
+    recv_from_pad: tokio::sync::mpsc::Receiver<PadResponse>,
 }
 #[tracing::instrument]
 pub async fn handle_stream(config: &Config, accept_result: Result<(UnixStream, SocketAddr), io::Error>, channels: &mut ServerChannels) {
@@ -23,11 +42,28 @@ pub async fn handle_stream(config: &Config, accept_result: Result<(UnixStream, S
             match stream.try_read(&mut msg) {
                 Ok(n) => {
                     debug!("Read {} bytes", n);
-                    let sample_msg = HardwareRequest::MotorWrite{motor: "motor1".to_string(), command: vec![0x2A, 0x08, 0xFF, 0xFF, 0x23]};
-                    let encoded_msg = serde_json::to_string(&sample_msg).unwrap();
-                    debug!("Encoded message: {:?}", encoded_msg);
+                    // let sample_msg = HardwareRequest::MotorWrite{motor: "motor1".to_string(), command: vec![0x2A, 0x08, 0xFF, 0xFF, 0x23]};
+                    // let encoded_msg = serde_json::to_string(&sample_msg).unwrap();
+                    // debug!("Encoded message: {:?}", encoded_msg);
                     let hw_req: HardwareRequest = serde_json::from_slice(&msg[..n]).unwrap();
                     debug!("Message: {:?}", hw_req);
+
+                    if let HardwareResponse::EncoderValue(v) = handle_request(config, hw_req, channels).await {
+                        let encoded_resp = serde_json::to_string(&v).unwrap();
+                        debug!("Encoded response: {:?}", encoded_resp);
+                        let resp = encoded_resp.as_bytes();
+                        stream.writable().await.unwrap();
+
+                        match stream.try_write(resp) {
+                            Ok(n) => {
+                                debug!("Wrote {} bytes", n);
+                            }
+                            Err(e) => {
+                                error!("Error writing to stream: {}", e);
+                            }
+                        }
+                    }
+
                 }
                 Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                     info!("Would block");
@@ -37,16 +73,28 @@ pub async fn handle_stream(config: &Config, accept_result: Result<(UnixStream, S
                 }
             }
 }
-async fn handle_request(config: &Config, req: HardwareRequest) {
+#[tracing::instrument]
+async fn handle_request(config: &Config, req: HardwareRequest, channels: &mut ServerChannels) -> HardwareResponse {
     match config.resolve(&req) {
         Some(Handler::Pad(port)) => {
-            println!("Pad port: {}", port);
+            let wait_for_response = matches!(req, HardwareRequest::EncoderRead{..});
+            let pad_req = PadRequest::from_hardware_request(port, req);
+            channels.send_to_pad.send(pad_req).await.unwrap();
+            if wait_for_response {
+                let pad_resp = channels.recv_from_pad.recv().await.unwrap();
+                debug!("Received pad response: {:?}", pad_resp);
+                HardwareResponse::from_pad_response(pad_resp)
+            } else {
+                HardwareResponse::Ok
+            }
         }
         Some(Handler::System(port)) => {
-            println!("System port: {}", port);
+            warn!("System port: {}, not implemented", port);
+            HardwareResponse::Ok
         }
         None => {
-            println!("No handler found");
+            warn!("No handler found");
+            HardwareResponse::Ok
         }
     }
 }
