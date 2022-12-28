47844f80b433e9214969fc9b73b055ed60593477
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-12-28 11:27:05 +0530

    Use mpsc instead of oneshot channels

--- src/server.rs
+++ src/server.rs
@@ -3,35 +3,37 @@ use serde::{Serialize, Deserialize};
 use std::io;
 use crate::config::{Config, Handler};
 use crate::pad;
+use tracing::{info, error, debug};
 
 #[derive(Serialize,Deserialize,Debug)]
 pub enum HardwareRequest {
     MotorWrite{motor: String, command: Vec<u8>},
     EncoderRead{encoder: String},
 }
+#[tracing::instrument]
 pub async fn handle_stream(config: &Config, accept_result: Result<(UnixStream, SocketAddr), io::Error>, pad_tx: &tokio::sync::oneshot::Sender<HardwareRequest>) {
     if let Err(e) = accept_result {
-        eprintln!("Error accepting connection: {}", e);
+        error!("Error accepting connection: {}", e);
         return;
     }
     let (stream, _addr) = accept_result.unwrap();
-            println!("New connection: {:?}", stream);
+            info!("New connection: {:?}", stream);
             stream.readable().await.unwrap();
             let mut msg = vec![0; 1024];
             match stream.try_read(&mut msg) {
                 Ok(n) => {
-                    println!("Read {} bytes", n);
+                    debug!("Read {} bytes", n);
                     let sample_msg = HardwareRequest::MotorWrite{motor: "motor1".to_string(), command: vec![0x2A, 0x08, 0xFF, 0xFF, 0x23]};
                     let encoded_msg = serde_json::to_string(&sample_msg).unwrap();
-                    println!("Encoded message: {:?}", encoded_msg);
+                    debug!("Encoded message: {:?}", encoded_msg);
                     let hw_req: HardwareRequest = serde_json::from_slice(&msg[..n]).unwrap();
-                    println!("Message: {:?}", hw_req);
+                    debug!("Message: {:?}", hw_req);
                 }
                 Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                    println!("Would block");
+                    info!("Would block");
                 }
                 Err(e) => {
-                    println!("Error: {:?}", e);
+                    error!("Error: {:?}", e);
                 }
             }
 }
