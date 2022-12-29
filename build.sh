09fd60ad223883118f49519ad45be98f016852ec
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-12-29 15:24:55 +0530

    Use nvidia prebuilt toolchain for cross-compile
    The aarch64-gnu-linux-gcc available on focal links
    against libudev wanting glibc 2.28, which is not
    available on TX2 running bionic. So use nvidia's
    prebuilt toolchain for TX2's precise L4T version.

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ export PKG_CONFIG_SYSROOT_DIR=${SYSROOT}
 export PKG_CONFIG_ALLOW_CROSS=1
 # tell pkg-config where to find libudev.pc
 export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig
-# tell cargo to link with an armhf compatible linker
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
+# tell cargo to link with an aarch64 compatible linker
+#export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc-8
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=/root/toolchain/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc
 
 cargo build --release --target=aarch64-unknown-linux-gnu
